c4d44495265989ed9685e7cb1ef2f4d1f14ab09a
hwnpac <angievivianabedoyaavila@gmail.com>
2026-05-26 17:59:19 -0500

    spa

---
+++ server.js
@@ -0,0 +1,17 @@
+require("dotenv").config();
+const express = require('express');
+const cors = require('cors');
+
+const routes = require('./routes');
+
+const app = express();
+
+app.use(cors());
+
+app.use(express.json());
+
+app.use('/api', routes);
+
+app.listen(3000, () => {
+  console.log('Servidor activo http://localhost:3000');
+});
